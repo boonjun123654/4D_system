d85ada1018af3c912e6a79640972a1a6d5c968ed
boonjun123654 <boonjunlee@gmail.com>
2025-07-07 23:31:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from odds_config import odds
+from datetime import datetime, timedelta
 
 value = odds["M"]["S"]["1st"]
 print("M 市场 小 投注 头奖赔率 =", value)
@@ -12,7 +13,8 @@ def index():
 
 @app.route('/bet')
 def bet():
-    return render_template('bet.html')
+    date_today = datetime.today()
+    return render_template('bet.html', date_today=date_today)
 
 @app.route('/report')
 def report():
