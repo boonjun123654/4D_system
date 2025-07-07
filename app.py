7d8cbd6c31c46e3835244835039a4dcf69fe58a9
boonjun123654 <boonjunlee@gmail.com>
2025-07-07 23:08:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
+from odds_config import odds
+
+value = odds["M"]["S"]["1st"]
+print("M 市场 小 投注 头奖赔率 =", value)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', odds=odds)
 
 @app.route('/bet')
 def bet():
