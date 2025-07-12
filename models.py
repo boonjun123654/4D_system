7332288566bce3e3436f7120fa646b7fd811d435
boonjun123654 <boonjunlee@gmail.com>
2025-07-12 11:55:30 +0800

    Update models.py

--- models.py
+++ models.py
@@ -18,7 +18,9 @@ class FourDBet(db.Model):
     markets = db.Column(db.ARRAY(db.Text), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
-class Agent(db.Model):
+class Agent4D(db.Model):
+    __tablename__ = 'agent_4d'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
