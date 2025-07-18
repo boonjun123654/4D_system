b1f4a433553d7fbd0effb2e1a248ef37de9a68e6
boonjun123654 <boonjunlee@gmail.com>
2025-07-19 00:55:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, session
+from flask import Flask, render_template, request, redirect, flash, session,jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from odds_config import odds
