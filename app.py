1e0438da3b13803b2872c563e3720a39d8f00d93
boonjun123654 <boonjunlee@gmail.com>
2025-07-26 00:15:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ app.secret_key = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'your-secret-key'
-csrf = CSRFProtect(app)
 
 login_attempts = {}
 
